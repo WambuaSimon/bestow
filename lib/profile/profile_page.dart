67e7d4bd0f5ba9027ddc0516ef3019e91a4a4415
simon.muia <simonwambua433@gmail.com>
2022-03-14 12:47:09 +0100

    profile header

--- lib/profile/profile_page.dart
+++ lib/profile/profile_page.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -11,9 +11,182 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    var appBarHeight = MediaQuery.of(context).size.height * 1 / 5;
+    return SafeArea(
+      child: Scaffold(
+          body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
+            height: appBarHeight,
+            decoration: const BoxDecoration(
+              color: Color(0xff209fa5),
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(30),
+              ),
+            ),
+          ),
 
-      body: Container(),
+          Container(
+            margin: const EdgeInsets.only(left: 15.0, bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: const Color(0xffF5F5F5),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Badge(
+                    badgeContent: const Text(
+                      "2",
+                      style: TextStyle(color: Colors.white, fontSize: 10),
+                    ),
+                    badgeColor: Colors.red,
+                    child: const Icon(
+                      Icons.favorite,
+                      color: Color(0xff209fa5),
+                      size: 20,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 30,),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: const Color(0xffF5F5F5),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Badge(
+                    badgeContent: const Text(
+                      "7",
+                      style: TextStyle(color: Colors.white, fontSize: 10),
+                    ),
+                    badgeColor: Colors.red,
+                    child: const Icon(
+                      Icons.notifications_sharp,
+                      color: Color(0xff209fa5),
+                      size: 20,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                PopupMenuButton<int>(
+                  itemBuilder: (context) => [
+                    PopupMenuItem(
+                      value: 1,
+                      child: Row(
+                        children: const [
+                          Icon(Icons.logout),
+                          SizedBox(width: 5,),
+                          Text("Logout")
+                        ],
+                      ),
+                    ),
+
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 1 / 9 ),
+            height: 150,
+            width: MediaQuery.of(context).size.width * 3 / 4 + 50,
+            child: Card(
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      maxRadius: 40,
+                      child: Image.asset("assets/images/user.png"),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: const [
+                            Text(
+                              "John Doe",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Color(0xff002642),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        const Text(
+                          "jdoe@gmail.com",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Color(0xff002642),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          "0714980450",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Color(0xff002642),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: const [
+                            Text(
+                              "Donations given: ",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Color(0xff002642),
+                              ),
+                            ),
+                            Text(
+                              "13",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff209fa5),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: const Alignment(0.9, -0.75),
+            child: Container(
+                margin: const EdgeInsets.only(
+                  left: 10,
+                  bottom: 10,
+
+                ),
+                child: const Icon(
+                  Icons.edit,
+                  size: 20,
+                )),
+          ),
+        ],
+      )),
     );
   }
 }
